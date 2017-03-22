b1be979bfd0f4fe1f382f18c2ae38d4866634f9f
Vince <git@darkain.com>
2017-03-22 11:27:37 -0700

    Helper script to check if needed PHP extensions are installed

---
+++ status/extensions.php
@@ -0,0 +1,39 @@
+<?php
+require_once('../error.php.inc');
+
+$list = get_loaded_extensions();
+
+$need = [
+	'ctype',		// cytpe_int
+	'curl',			// read/write HTTP(s) GET/POST streams
+	'exif',			// photo metadata
+	'filter',		// IPv4/IPv6 filter
+	'hash',			// ??? - encryption
+	'iconv',		// UTF-8 conversion
+	'imagick',		// photo processing
+	'json',			// javascript object notation
+//	'mail',			// error reporting and user account registration emails
+	'mcrypt',		// ??? - encryption
+	'mysqli',		// database
+	'openssl',		// fopen('https//example.com') HTTPS
+	'redis',		// database caching
+	'session',		// ???
+	'sockets',		// ???
+	'tokenizer',	// ???
+	'xml',			// ???
+	'zlib',			// gzip compress static files
+];
+
+foreach ($need as $item) {
+	assert500(
+		in_array($item, $list),
+		'Required PHP module not found: ' . $item
+	);
+}
+
+
+if (!empty($af)) {
+	$af->ok();
+} else {
+	echo "GOOD!\n";
+}
