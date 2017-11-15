d47f344777ed706eee4d46ad52de725d3b8f9068
Vince <git@darkain.com>
2017-11-15 09:56:52 -0800

    Storage location is now standardized

--- upload/_index.php
+++ upload/_index.php
@@ -13,21 +13,24 @@ assert500(
 );
 
 
-//TODO: STR_REPLACE IS A TEMPORARY FIX.
-//THE UPLOADER SHOULD RECOGNIZE OUR CDN LOCATION BY DEFAULT, BUT DOESNT YET
+$path = false;
+
+
 if (!empty($data[500]['url'])) {
-	return $af->json([
-		'location' => str_replace('cdn/', 'files/', $data[500]['url']),
-	]);
+	$path = $data[500]['url'];
 }
 
 
-//TODO: OUTPUT SHOULD OPTIONALLY BE JSON FOR TINYMCE, OR AF STYLE FOR COSPIX
 if (!empty($data['url'])) {
-	return $af->json([
-		'location' => str_replace('cdn/', 'files/', $data['url']),
-	]);
+	$path = $data['url'];
 }
 
 
-error500('Unknown error while processing image file');
+assert500($path, 'Unknown error while processing image file');
+
+
+if (!empty($afconfig->cdn['local'])) {
+	$path = str_replace('cdn/', $afconfig->cdn['local'].'/', $path);
+}
+
+return $af->json(['location' => $path]);
