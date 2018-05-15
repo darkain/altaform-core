9d6cd23e13d7b81357df06e6d8603f5043c9191d
Vince <git@darkain.com>
2018-05-15 11:14:57 -0700

    Adding case-insensitive version of in_array as in_arrayi

--- core/afFunctions.inc.php
+++ core/afFunctions.inc.php
@@ -1,8 +1,26 @@
 <?php
 
 
+////////////////////////////////////////////////////////////////////////////////
 //FALL BACK IF random_int() DOESN'T EXIST - NEW IN PHP 7.0
 //THIS IS USED FOR PASSWORD GENERATOR
+////////////////////////////////////////////////////////////////////////////////
 if (!function_exists('random_int')) {
 	function random_int($min, $max) { return mt_rand($min, $max); }
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//CASE INSENSITIVE IN_ARRAY()
+//SOURCE: http://us2.php.net/manual/en/function.in-array.php#89256
+////////////////////////////////////////////////////////////////////////////////
+if (!function_exists('in_arrayi')) {
+	function in_arrayi($needle, $haystack) {
+		return in_array(
+			strtolower($needle),
+			array_map('strtolower', $haystack)
+		);
+	}
 }
