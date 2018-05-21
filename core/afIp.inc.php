a5dc3565f88fdefc75edde27b0fb8f335566e542
Vince <git@darkain.com>
2018-05-21 14:26:14 -0700

    Fixing bug with trying to create socket with php socket module isnt installed

--- core/afIp.inc.php
+++ core/afIp.inc.php
@@ -85,10 +85,12 @@ class afIp {
 	////////////////////////////////////////////////////////////////////////////
 	public static function local() {
 		$address	= false;
-		$socket		= @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
-		if ($socket !== false) {
-			@socket_connect(	$socket, '8.8.8.8', 53);
-			@socket_getsockname($socket, $address);
+		if (function_exists('socket_create')) {
+			$socket	= @socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+			if ($socket !== false) {
+				@socket_connect(	$socket, '8.8.8.8', 53);
+				@socket_getsockname($socket, $address);
+			}
 		}
 		return $address;
 	}
