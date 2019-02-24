9040744830098ee09d1de934c1ff9172242c5558
Vincent Milum Jr <git@darkain.com>
2019-02-24 01:44:32 -0800

    FreeBSD package installer

---
+++ setup/freebsd-packages.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+pkg install -y php73 php73-curl php73-exif php73-sodium php73-zip php73-zlib php73-mysqli php73-pecl-imagick php73-pecl-redis php73-sysvmsg php73-sysvsem php73-sysvshm php73-sockets php73-simplexml php73-extensions
