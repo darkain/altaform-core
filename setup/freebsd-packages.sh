f4a5cbead5b2972a7267dcab4ee2f690197e2b24
Vincent Milum Jr <git@darkain.com>
2019-02-24 02:00:26 -0800

    Enable and start the PHP-FPM daemon automatically

--- setup/freebsd-packages.sh
+++ setup/freebsd-packages.sh
@@ -5,3 +5,11 @@ pkg install -y php73 php73-curl php73-exif php73-sodium php73-zip php73-zlib php
 
 # ENABLE PHP-FPM ON ALL IPv4 ADDRESSES
 sed -i .altaform 's/127.0.0.1:9000/0.0.0.0:9000/' /usr/local/etc/php-fpm.d/www.conf
+
+# ENABLE THE PHP-FPM DAEMON
+LINE='php_fpm_enable="YES"'
+FILE=/etc/rc.conf.local
+grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+
+# START THE PHP-FPM DAEMON
+service php-fpm start
