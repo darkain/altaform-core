ffe503acbc700deb1c83a6901fa5b1d65c70e34e
Vince <git@darkain.com>
2019-05-14 11:16:54 -0700

    Add error checking to Encrypt methods Added in some minor extra security features

--- traits/afEncrypt.inc.php
+++ traits/afEncrypt.inc.php
@@ -12,17 +12,23 @@ trait afEncrypt {
 	public function encrypt($data) {
 		$this->checkExtension('openssl');
 
+		if (empty($this->config->encrypt['cipher'])  ||
+			empty($this->config->encrypt['key'])  ||
+			is_null($this->{'af.encrypt'})) {
+			throw new afException('Encryption cipher or key not set');
+		}
+
 		$data	= json_encode($data);
-		$ivlen	= openssl_cipher_iv_length($this->config->pci['cipher']);
+		$ivlen	= openssl_cipher_iv_length($this->config->encrypt['cipher']);
 		$iv		= openssl_random_pseudo_bytes($ivlen);
 		$hash	= hash_hmac('sha256', $data, $iv);
 
 		return [
-			'iv'	=> base64_encode($iv),
+			'iv'	=> rtrim(base64_encode($iv), '='),
 			'raw'	=> openssl_encrypt(
 				$hash . ':' . $data,
-				$this->config->pci['cipher'],
-				$this->config->pci['key'] . $this->{'af.pci'},
+				$this->config->encrypt['cipher'],
+				$this->config->encrypt['key'] . $this->{'af.encrypt'},
 				$options=0,
 				$iv
 			),
@@ -38,26 +44,28 @@ trait afEncrypt {
 	public function decrypt($encrypted) {
 		$this->checkExtension('openssl');
 
+		if (empty($this->config->encrypt['cipher'])  ||
+			empty($this->config->encrypt['key'])  ||
+			is_null($this->{'af.encrypt'})) {
+			throw new afException('Encryption cipher or key not set');
+		}
+
 		if (empty($encrypted['iv'])  ||  empty($encrypted['raw'])) return NULL;
 
 		$iv = @base64_decode($encrypted['iv']);
 
 		$decrypted = @openssl_decrypt(
 			$encrypted['raw'],
-			$this->config->pci['cipher'],
-			$this->config->pci['key'] . $this->{'af.pci'},
+			$this->config->encrypt['cipher'],
+			$this->config->encrypt['key'] . $this->{'af.encrypt'},
 			$options=0,
 			$iv
 		);
 
-		$pos	= strpos($decrypted, ':');
-		if (empty($pos)) return NULL;
+		@list($hash, $string) = explode(':', $decrypted, 2);
 
-		$string	= substr($decrypted, $pos+1);
-		$hash	= substr($decrypted, 0, $pos);
-
-		return ($hash === hash_hmac('sha256', $string, $iv))
-				? json_decode($string, true)
+		return (hash_equals($hash, hash_hmac('sha256', $string, $iv)))
+				? @json_decode($string, true)
 				: NULL;
 	}
 
