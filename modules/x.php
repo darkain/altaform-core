a081c451911062fe93fc38a2c368b606e19459f6
Vince <git@darkain.com>
2022-09-12 22:26:20 -0700

    minor change to notes

--- modules/x.php
+++ modules/x.php
@@ -3,7 +3,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // FALL BACK IF random_int() DOESN'T EXIST - NEW IN PHP 7.0
-// THIS IS A POLYFILL FOR PHP 5.X - REALLY THOUGH, PLEASE UPGRADE TO 7.X
+// THIS IS A POLYFILL FOR PHP 5.X - REALLY THOUGH, PLEASE UPGRADE TO 8.X
 /** @suppress PhanRedefineFunctionInternal */
 ////////////////////////////////////////////////////////////////////////////////
 if (!function_exists('random_int')) {
